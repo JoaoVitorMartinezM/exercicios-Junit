fb338f3b5d742fe364aa756ecfc97450c8424bae
Joao Vitor Martinez <justdatascientist@gmail.com>
2023-04-22 20:18:20 -0300

    Model e config do H2

---
+++ src/main/java/com/exercicios/exerciciostestesunitarios/models/Veiculos.java
@@ -0,0 +1,27 @@
+package com.exercicios.exerciciostestesunitarios.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Entity(name = "VEICULOS")
+public class Veiculos {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    String placa;
+
+    String tipo;
+    String cor;
+    Integer anoDeFabricacao;
+    Integer qtdMultas;
+
+
+
+}
